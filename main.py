4de186de7406e6e81e4e1cadca0105448b25c7d1
BuildTools <unconfigured@null.spigotmc.org>
2024-03-04 21:15:12 +0100

    Dodano komentarze

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+# Imports
 import tkinter as tk
 import tkinter.ttk as ttk
 import pyqrcodeng
 from time import sleep
 
+# Class
 class GuiApp:
     def __init__(self, master=None):
+
+        # QR Code Generator
         def qrgen():
             url = pyqrcodeng.create(linkbox.get())
             url.png(namebox.get() + '.png', scale = 6)
 
+        # Main window
         self.window = tk.Tk() if master is None else tk.Toplevel(master)
         self.window.configure(background="#525f98", height=200, width=200)
         self.window.geometry("320x200")
         self.window.resizable(False, False)
+
+        # First label
         label1 = ttk.Label(self.window)
         label1.configure(
             anchor="center",
@@ -29,6 +36,8 @@ class GuiApp:
             rely=0.0,
             x=0,
             y=0)
+
+        # Second label
         label2 = ttk.Label(self.window)
         label2.configure(
             background="#525f98",
@@ -36,6 +45,8 @@ class GuiApp:
             foreground="#ffffff",
             text='Your Link/numbers(or anything else)')
         label2.place(anchor="center", relx=0.5, rely=0.31, x=0, y=0)
+
+        # Entry for link
         linkbox = ttk.Entry(self.window, name="linkbox")
         linkbox.place(
             anchor="center",
@@ -44,6 +55,8 @@ class GuiApp:
             rely=0.42,
             x=0,
             y=0)
+
+        # Third label
         label3 = ttk.Label(self.window)
         label3.configure(
             background="#525f98",
@@ -51,6 +64,8 @@ class GuiApp:
             foreground="#ffffff",
             text='Name of your QR Code file')
         label3.place(anchor="center", relx=0.5, rely=0.55, x=0, y=0)
+
+        # Entry for file name
         namebox = ttk.Entry(self.window, name="namebox")
         namebox.place(
             anchor="center",
@@ -59,6 +74,8 @@ class GuiApp:
             rely=0.66,
             x=0,
             y=0)
+
+        # Button for generation
         generatebutton = ttk.Button(self.window, name="generatebutton")
         generatebutton.configure(text='GENERATE', command = qrgen)
         generatebutton.place(
@@ -72,6 +89,7 @@ class GuiApp:
         # Main widget
         self.mainwindow = self.window
 
+    # mainloop
     def run(self):
         self.mainwindow.mainloop()
 
